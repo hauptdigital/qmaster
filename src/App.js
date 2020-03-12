6192f150758cbb22f1c474c9a1f028c904c73ff4
Marc <marc@haupt.digital>
2020-03-12 14:50:47 +0100

    Install prettier and prettify all .js files (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Add from "./pages/Add";
-import Vote from "./pages/Vote";
-import Result from "./pages/Result";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Add from './pages/Add';
+import Vote from './pages/Vote';
+import Result from './pages/Result';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
