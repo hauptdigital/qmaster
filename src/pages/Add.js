08f3398277af9ee9611ed98707b08f98edb54237
hauptdigital <marc@haupt.digital>
2020-03-17 14:37:13 +0100

    Post to fake API on submit

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -34,7 +34,7 @@ function Add() {
   const [answerTwo, setAnswerTwo] = React.useState('');
   const [answerThree, setAnswerThree] = React.useState('');
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
     const poll = {
@@ -44,7 +44,16 @@ function Add() {
       answerThree: answerThree
     };
 
-    alert(JSON.stringify(poll));
+    const response = await fetch('http://localhost:4000/polls', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(poll)
+    });
+    const createdPoll = await response.json();
+    console.log(createdPoll);
+    alert(`Created poll with the id ${createdPoll.id}`);
   }
 
   return (
