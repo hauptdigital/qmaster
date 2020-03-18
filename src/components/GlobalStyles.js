220bc4f4fa5ecf8038ed12d0460218c4ec5ab627
hauptdigital <marc@haupt.digital>
2020-03-18 15:02:34 +0100

    Remove not needed theme file and object

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
-
-const theme = {
-  colors: {
-    primary: 'white',
-    secondary: 'rgba(0, 0, 0, 0.5)',
-    secondaryHover: 'rgba(0, 0, 0, 0.25)',
-    gradient:
-      'linear-gradient(45deg,#405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d)'
-  }
-};
+import dark from '../themes/dark';
 
 function GlobalStyles() {
   return (
@@ -24,7 +15,7 @@ function GlobalStyles() {
 
           body {
             margin: 0;
-            background: ${theme.colors.gradient};
+            background: ${dark.colors.gradient};
             height: 100vh;
             font-family: Arial, Helvetica, sans-serif;
           }
